3991bec554811841c13170bb2f73133a4f3bfcee
Shaymaa Hasson <67877735+Shaymaa48@users.noreply.github.com>
2022-08-29 15:48:06 +0300

    Finish chat UI

--- index.php
+++ index.php
@@ -15,7 +15,45 @@
 	.chat-input{border: 1px solid #ffdddd;border-top: 0px;width: 100%;box-sizing: border-box;padding: 10px 8px;color: #191919;
 	}
 	</style>	
+	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+	<script>  
+	function showMessage(messageHTML) {
+		$('#chat-box').append(messageHTML);
+	}
 
+	$(document).ready(function(){
+		var websocket = new WebSocket("ws://localhost:8090/demo/php-socket.php"); 
+		websocket.onopen = function(event) { 
+			showMessage("<div class='chat-connection-ack'>Connection is established!</div>");		
+		}
+		websocket.onmessage = function(event) {
+			var Data = JSON.parse(event.data);
+			showMessage("<div class='"+Data.message_type+"'>"+Data.message+"</div>");
+			$('#chat-message').val('');
+		};
+		
+		websocket.onerror = function(event){
+			showMessage("<div class='error'>Problem due to some Error</div>");
+		};
+		websocket.onclose = function(event){
+			showMessage("<div class='chat-connection-ack'>Connection Closed</div>");
+		}; 
+		
+		$('#frmChat').on("submit",function(event){
+			event.preventDefault();
+			$('#chat-user').attr("type","hidden");		
+			var messageJSON = {
+				chat_user: $('#chat-user').val(),
+				chat_message: $('#chat-message').val()
+			};
+			websocket.send(JSON.stringify(messageJSON));
+		});
+	});
+
+
+
+
+	</script>
 	</head>
 	<body>
 		<form name="frmChat" id="frmChat">
